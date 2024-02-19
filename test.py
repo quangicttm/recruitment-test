723848a979fcdcdb2b8ce6d140fa15e28b06e505
Q.T.N <quangtn24198@gmail.com>
2024-02-19 20:33:13 +0700

    Add requirements

---
+++ test.py
@@ -0,0 +1,15 @@
+'''
+How do we test the code?
+- Using Python and the libraries can be used to solve the problem.
+- Clean code.
+- Documentation what the code does and how to run it (in the code)
+- The result of the code should be returned in a new file (not printed in the console - can be a .csv file)
+
+Requirements:
+- Write a function that downloads the data from this URL: https://drive.google.com/uc?id=1yyL20BNKv3PxJRJVjJ_2Q-HidvIUis45&export=download (file .zip) 
+    and saves it in the data directory
+- Extract the .zip file and read the .csv file inside data directory
+- Write a function that returns the number of rows and columns of the .csv file
+- Write a function to display specific columns as follows: index, customer_id, first_name, last_name, phone_1 from the file you extracted about . 
+    Save the result in a new .csv file.
+'''
